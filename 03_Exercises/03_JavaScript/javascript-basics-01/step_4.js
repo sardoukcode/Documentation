b75032224561a69ee4e3cc4965ffe30cee5765ed
faysal-sardouk <sardouk29@gmail.com>
2022-12-07 18:49:39 +0200

    basic 1 task 4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_4.js
@@ -0,0 +1,4 @@
+var NameUser = prompt("what's your name?");
+var LastUser = prompt("what's your last name?");
+var CityUser = prompt("where are you from?");
+alert("hello " + NameUser + LastUser + "from " + CityUser);
