92dde4dd18f579147330d8b7665f1012acf33dbc
faysal-sardouk <sardouk29@gmail.com>
2022-12-08 12:57:05 +0200

    basic 1 task7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_7.js
@@ -0,0 +1,6 @@
+function Showing() {
+  var Num1 = document.getElementById("shoe_size").value;
+  var Num2 = document.getElementById("year").value;
+  var Total = Num1 * 2 + 5 - Num2 + 1766;
+  alert(Total);
+}
