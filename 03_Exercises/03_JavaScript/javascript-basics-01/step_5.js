28a8864e8b0edafae1b2a885e448496b30465149
faysal-sardouk <sardouk29@gmail.com>
2022-12-08 12:09:17 +0200

    basic 1 step 5

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_5.js
@@ -0,0 +1,5 @@
+function Showing() {
+  var Num1 = document.getElementById("first_number").value;
+  var Num2 = document.getElementById("second_number").value;
+  alert("THE TOTAL IS " + Num1 * Num2);
+}
