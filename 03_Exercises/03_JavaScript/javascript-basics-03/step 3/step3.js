7be933c548f94e828cc857bda07217a7608c8de2
faysal-sardouk <sardouk29@gmail.com>
2022-12-09 14:06:23 +0200

    step3

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 3/step3.js
@@ -0,0 +1,11 @@
+function colorGreen() {
+  document.getElementById("text").style.color = "green";
+}
+
+function colorRed() {
+  document.getElementById("text").style.color = "red";
+}
+
+function colorBlue() {
+  document.getElementById("text").style.color = "blue";
+}
