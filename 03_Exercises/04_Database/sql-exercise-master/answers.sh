5b848d4c0e9ea6b08ec60252ac57004c136ee3a5
faysal-sardouk <sardouk29@gmail.com>
2022-12-11 16:36:41 +0200

    first task

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,12 @@
+1- SELECT * FROM 'students'
+2-SELECT * FROM 'students' where age>30;
+3-SELECT * FROM 'students' where gender = "F" AND age>30
+4-SELECT Points FROM students WHERE name="Alex"
+5-INSERT INTO students (ID, name, Age,Gender,Points)
+VALUES ( 7 , 'faysal',27,'M', 600)
+6-UPDATE students
+SET Points= 450
+WHERE ID=2;
+7-UPDATE students
+SET Points= 150
+WHERE ID=1;
