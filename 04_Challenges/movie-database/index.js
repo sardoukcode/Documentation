4569f2f61cd73532206602684ac8f46d5ae546a5
faysal-sardouk <sardouk29@gmail.com>
2023-01-08 18:51:48 +0200

    step 2

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 2000
+const port = 3000
+
+
+
+
+
+
+app.get('/test', (req, res) => {
+    res.send("ok")
+
+});
+
+app.get('/time', (req, res) => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const seconds = currentTime.getSeconds();
+
+    res.send(hours + ":" + seconds )
+  });
+
+
+
 
 app.get('/', (req, res) => {
   res.send('ok')
+  res.send('okkkkkk')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
