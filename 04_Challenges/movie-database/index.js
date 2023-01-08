2d0c1f8ce397bc6c983dff599c05599d3ed5e8d4
faysal-sardouk <sardouk29@gmail.com>
2023-01-08 18:57:06 +0200

    step 5

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,51 +1,65 @@
-const express = require('express')
-const express = require('express');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
-const app = express()
-const port = 3000
+@@ -6,13 +6,22 @@ const port = 3000
+
+
+
+const movies = [ { title: 'Jaws', year: 1975, rating: 8 }, { title: 'Avatar', year: 2009, rating: 7.8 }, { title: 'Brazil', year: 1985, rating: 8 }, { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 } ]
+
 
 
 
 app.get('/', (req, res) => {
+    res.status(200);
+
     res.send('Wellcome')
   })
-
+})
 
 
 // Test command
 app.get('/test', (req, res) => {
+    res.status(200);
+
     res.send("ok")
 
 });
-
-// Time command
-app.get('/time', (req, res) => {
+@@ -21,22 +30,28 @@ app.get('/time', (req, res) => {
     const currentTime = new Date();
     const hours = currentTime.getHours();
     const seconds = currentTime.getSeconds();
 
-    res.send(hours + ":" + seconds )
     res.send("so: " + hours + ":" + seconds )
   });
+    res.status(200);
+
+
+    res.send("so: " + hours + ":" + seconds)
+});
 
 // hello command
   app.get('/hello/:id', (req, res) => {
+app.get('/hello/:id', (req, res) => {
     const id = req.params.id;
+    res.status(200);
+
     res.send(`Hello,${id}`);
   });  
   app.get('/hello', (req, res) => {
+});
+app.get('/hello', (req, res) => {
     const id = req.params.id;
+    res.status(200);
+
     res.send(`Hello`);
   });
+});
 
 //   search command
 app.get('/search',(req,res) => {
+app.get('/search', (req, res) => {
     const search = req.query.s;
 
     if (typeof search != 'undefined') {
-        // Search string applied
-        res.status(200);
+@@ -45,23 +60,45 @@ app.get('/search',(req,res) => {
         res.send(`ok, data: ${search}`);
     }
     else {
@@ -60,14 +74,40 @@ app.get('/search',(req,res) => {
 
 
 
+// create commad
+app.get('/movies/create', (req, res) => {
+    res.send("create")
+
+});  
+//  read command
+app.get('/movies/read', (req, res) => {
+    res.status(200);
+    res.send( movies )
+
+
+});
+// update command
+app.get('/movies/update', (req, res) => {
+    res.send("update")
+
+});
+// delete command
+app.get('/movies/delete', (req, res) => {
+    res.send("delete")
+
+});
 
 
 
-app.get('/', (req, res) => {
-  res.send('okkkkkk')
-})
+
+
+
+
+
 
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
