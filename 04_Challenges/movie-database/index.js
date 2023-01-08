7f5443f6d7301f59440f0965f4c884846ad2119a
faysal-sardouk <sardouk29@gmail.com>
2023-01-08 18:59:12 +0200

    step 7

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,50 +1,36 @@
-const movies = [ { title: 'Jaws', year: 1975, rating: 8 }, { title: 'Avatar', year: 2009, rating: 7.8 }, { title: 'Brazil', year: 1985, rating: 8 }, { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 } ]
 const movies = [{ title: 'Jaws', year: 1975, rating: 8 }, { title: 'Avatar', year: 2009, rating: 7.8 }, { title: 'Brazil', year: 1985, rating: 8 }, { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }]
+const movies = [
+    {id:1, title: 'Jaws', year: 1975, rating: 8 },
+    {id:2, title: 'Avatar', year: 2009, rating: 7.8 },
+    {id:3, title: 'Brazil', year: 1985, rating: 8 },
+    {id:4, title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
 
 
 
-@@ -70,14 +70,43 @@ app.get('/search', (req, res) => {
-app.get('/movies/create', (req, res) => {
-    res.send("create")
-
-});  
-});
-//  read command
-app.get('/movies/read', (req, res) => {
-    res.status(200);
-    res.send( movies )
-
-    res.send(movies)
-
-
-});
-
-// ORDERED BY DATE
-
-app.get('/movies/read/by-date', (req, res) => {
-
-    const sortedMovies = movies.sort((a, b) => a.year - b.year);
-    res.status(200);
-    res.send(sortedMovies);
-});
-
-
-//   ORDERED BY RATING
-
-app.get('/movies/read/by-rating', (req, res) => {
-
-    const sortedMovies = movies.sort((a, b) => b.rating - a.rating);
-    res.status(200);
-    res.send(sortedMovies);
-});
-
-// ORDERED BY TITLE
-
-app.get('/movies/read/by-title', (req, res) => {
-    const sortedMovies = movies.sort((a, b) => a.title.localeCompare(b.title));
-    res.status(200);
-    res.send( sortedMovies);
+@@ -107,6 +112,27 @@ app.get('/movies/read/by-title', (req, res) => {
   });
+
+
+
+//   movie defined by the ID
+
+  app.get('/movies/read/id/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    const movie = movies.find(movie => movie.id === Number(id));
+
+    if (movie) {
+      res.status(200);
+      res.send(movie)
+    } else {
+      res.status(404);
+      res.send(`the movie ${id} does not exist` );
+    }
+  });
+
+
 
 
 // update command
